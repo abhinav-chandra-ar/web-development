802612dc6561be4febcb999af02b42cc81f73ff4
chadxabhinav <abhinavcanichandran@gmail.com>
2026-06-03 20:46:36 +0530

    Express generator

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-const http = require("http");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const server = http.createServer((req, res) => {
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-  if (req.url === "/") {
-    res.write("Welcome to Home Page");
-    res.end();
-  }
+var app = express();
 
-  else if (req.url === "/about") {
-    res.write("Welcome to About Page");
-    res.end();
-  }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-  else if (req.url === "/contact") {
-    res.write("Welcome to Contact Page");
-    res.end();
-  }
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  else {
-    res.write("404 Page Not Found");
-    res.end();
-  }
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
